4c8c5d7d581067035a9b0cd27575bd9936881087
Collins Njau <collinsnjau39@gmail.com>
2018-07-13 14:20:44 +0300

    Put data into a list

--- scrapper.py
+++ scrapper.py
@@ -14,7 +14,28 @@ class Scrapper(object):
 
         # first ping the requested url and check the response
         response = requests.get(self.url)
-        return response
+        # parse the contents of the response into html
+        html = BeautifulSoup(response.content, 'html.parser')
+        if html :
+            # get all div tags with the class col-xs-6 col-md-6 zero
+            div = html.find('div', class_='col-xs-6 col-md-6 zero')
+            ul = div.find_all('ul', class_='sub-stories-2')
+            data = []
+            for item in ul:
+                image_url = item.find('img').get('src')
+                text = item.find('img').get('alt')
+                link = item.find('li').find('a').get('href')
+            data.append({
+                'image':image_url,
+                'text': text,
+                'link': link
+            })
+            return json.dumps(data, indent=2)    
+            
+
+
+
+
 
 scraper = Scrapper()
 print(scraper.scrape_website())
