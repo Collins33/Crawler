dc2ce025f3b688ac68c6b6c4e0010eb43b63bd9a
Collins Njau <collinsnjau39@gmail.com>
2018-07-13 11:31:01 +0300

    Check response from the passed url

--- scrapper.py
+++ scrapper.py
@@ -4,3 +4,18 @@ import requests
 
 from bs4 import BeautifulSoup
 
+class Scrapper(object):
+
+    def __init__(self):
+        self.url = "https://www.standardmedia.co.ke"
+
+    def scrape_website(self):
+        # the method that scrapes the website
+
+        # first ping the requested url and check the response
+        response = requests.get(self.url)
+        return response
+
+scraper = Scrapper()
+print(scraper.scrape_website())
+            
